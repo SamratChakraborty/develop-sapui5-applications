fcbe40da5bce10773e4a4f907e43e47ad1aa98a5
Samrat Chakraborty <csamrat53@gmail.com>
2025-02-12 08:59:05 +0000

    Models and Data Binding-Implementing Element Binding

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -31,6 +31,10 @@ sap.ui.define([
             },
             onCloseDialog: function () {
                 this.byId("dialog").close();
+            },
+            onCustomerChange: function (oEvent) {
+                var oBindingContext = oEvent.getParameter("listItem").getBindingContext();
+                this.byId("bookingTable").setBindingContext(oBindingContext);
             }              
 
         });
